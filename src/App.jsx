fd54e4cc0eb0071b86bed12b9be6169630945a49
zackad <zackad.dev@gmail.com>
2018-08-27 12:57:34 +0700

    put header and footer in main app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import Footer from './components/Footer'
+import NavbarApp from './components/NavbarApp'
 import Homepage from './components/contents/Homepage'
 import { HashRouter as Router } from 'react-router-dom'
 
@@ -8,7 +10,9 @@ class App extends Component {
     return (
       <Router>
         <div className='App'>
+          <NavbarApp />
           <Homepage />
+          <Footer />
         </div>
       </Router>
     )
