96d0e5befff7c8feaf5f5818c4bebc9b50852f96
zackad <zackad.dev@gmail.com>
2018-08-27 15:26:42 +0700

    remove unused props from Map component

--- src/components/contents/Peta.jsx
+++ src/components/contents/Peta.jsx
@@ -15,7 +15,7 @@ export default class Peta extends Component {
     const position = [this.state.lat, this.state.lng]
     return (
       <Container className='mt-3'>
-        <Map center={position} zoom={this.state.zoom} id='map'>
+        <Map center={position} zoom={this.state.zoom}>
           <TileLayer
             attribution="<a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
