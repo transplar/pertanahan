ec3adb8ce058977471132131ac23ea2a02f3b439
zackad <zackad.dev@gmail.com>
2018-08-29 11:51:45 +0700

    simplify copyright notice on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
   <footer className='bg-dark text-light mt-4'>
     <Container>
       <Col className='py-3 text-center'>
-        Copyright © 2018
+        <span className='d-none d-sm-inline'>Copyright</span> © 2018
         <a href='http://disperkim.jabarprov.go.id'>
           <span className='d-none d-sm-inline'> Dinas Perumahan Dan Permukiman Jawa Barat</span>
           <span className='d-inline d-sm-none'> DISPERKIM JABAR</span>
