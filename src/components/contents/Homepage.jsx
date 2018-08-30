59c262588b6a98f3bdffb45080567a14d4072268
zackad <zackad.dev@gmail.com>
2018-08-30 17:42:27 +0700

    sub bidang information as card

--- src/components/contents/Homepage.jsx
+++ src/components/contents/Homepage.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import {
-  Button,
   Col,
   Container,
   Row
 } from 'reactstrap'
+import { Bidang } from '../cards/Bidang'
+import BidangData from '../cards/bidang.json'
 
 class Homepage extends Component {
   render () {
@@ -12,9 +13,19 @@ class Homepage extends Component {
       <main className='p-0'>
         <Container className='py-4'>
           <Row>
-            <Col>
-              <Button color='danger'>Danger!</Button>
-            </Col>
+            {
+              BidangData.map((card, i) => (
+                <Col sm='6'>
+                  <Bidang
+                    title={card.title}
+                    subtitle={card.subtitle}
+                    text={card.text}
+                    button={card.button}
+                    className='mb-4'
+                    key={i} />
+                </Col>
+              ))
+            }
           </Row>
         </Container>
       </main>
