e696bff72e466f3b71b3a9e97083fdb8e5bec16f
zackad <zackad.dev@gmail.com>
2018-09-04 13:58:07 +0700

    add header into app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Homepage from './components/layout/Homepage'
 import Footer from './components/Footer'
+import Header from './components/Header'
 import './style.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
+        <Header />
         <Homepage />
         <Footer />
       </div>
