865fb51c0a92a1b93e1cbedb9a2abb7c493cfb24
zackad <zackad.dev@gmail.com>
2018-09-04 10:51:14 +0700

    get rid of rounded border on card bidang component

--- src/components/cards/Bidang.jsx
+++ src/components/cards/Bidang.jsx
@@ -10,8 +10,8 @@ import {
 } from 'reactstrap'
 
 export const Bidang = (props) => (
-  <Card className={props.className}>
-    <CardImg top src='https://placeholdit.imgix.net/~text?txtsize=33&txt=Image' alt='Card image cap'></CardImg>
+  <Card className={`${props.className} rounded-0`}>
+    <CardImg top className='rounded-0' src='https://placeholdit.imgix.net/~text?txtsize=33&txt=Image' alt='Card image cap'></CardImg>
     <CardBody>
       <CardTitle>{props.title}</CardTitle>
       <CardSubtitle>{props.subtitle}</CardSubtitle>
