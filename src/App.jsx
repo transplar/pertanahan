81482c744b59b96e4167220ec562c6e0024cae54
zackad <zackad.dev@gmail.com>
2018-09-04 13:30:21 +0700

    add footer site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Homepage from './components/layout/Homepage'
+import Footer from './components/Footer'
+import './style.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <Homepage />
+        <Footer />
       </div>
     )
   }
