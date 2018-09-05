f8f257716f5bbde9f324daa5392473895c972ff0
zackad <zackad.dev@gmail.com>
2018-09-05 10:27:16 +0700

    wrap App component in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { HashRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import Homepage from './components/layout/Homepage'
 import Footer from './components/Footer'
@@ -8,11 +9,13 @@ import './style.css'
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <Header />
-        <Homepage />
-        <Footer />
-      </div>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Homepage />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
