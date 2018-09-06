c080b5d9dfcb58aae2120980c881e66459927fce
zackad <zackad.dev@gmail.com>
2018-09-06 14:38:51 +0700

    add carousel slide component

--- src/components/layout/Homepage.jsx
+++ src/components/layout/Homepage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Container } from 'reactstrap'
 import NavbarApp from '../NavbarApp'
+import MainSlide from '../MainSlide'
 
 class Homepage extends Component {
   render() {
     return (
       <Container>
+        <MainSlide />
         <NavbarApp />
       </Container>
     )
