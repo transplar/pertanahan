bce63e674435968191011d7c4a85310ca392cdf7
zackad <zackad.dev@gmail.com>
2018-09-06 15:56:40 +0700

    add twitter-widget to homepage

--- src/components/layout/Homepage.jsx
+++ src/components/layout/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Container } from 'reactstrap'
+import { Container, Col, Row } from 'reactstrap'
+import { Timeline } from 'react-twitter-widgets'
 import NavbarApp from '../NavbarApp'
 import MainSlide from '../MainSlide'
 
@@ -9,6 +10,22 @@ class Homepage extends Component {
       <Container>
         <MainSlide />
         <NavbarApp />
+        <Row>
+          <Col md='8'></Col>
+          <Col>
+            <div className='border border-success p-1 mt-3'>
+              <div className='text-center text-white bg-main-color py-2'>TWITTER</div>
+              <Timeline
+                dataSource={{
+                  sourceType: 'profile',
+                  screenName: 'jawabarat'
+                }}
+                options={{
+                  height: '400'
+                }}/>
+              </div>
+          </Col>
+        </Row>
       </Container>
     )
   }
