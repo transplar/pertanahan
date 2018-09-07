ac5afd3b4e0ec665faf5fdcf4320e4f0c1c08d90
zackad <zackad.dev@gmail.com>
2018-09-07 15:51:04 +0700

    recomposition Homepage component

--- src/components/layout/Homepage.jsx
+++ src/components/layout/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Container, Col, Row } from 'reactstrap'
 import { Timeline } from 'react-twitter-widgets'
+import Header from '../Header'
+import Footer from '../Footer'
 import NavbarApp from '../NavbarApp'
 import MainSlide from '../MainSlide'
 import StatistikWidget from '../StatistikWidget'
@@ -11,39 +13,43 @@ import BeritaList from '../BeritaList';
 class Homepage extends Component {
   render() {
     return (
-      <Container>
-        <MainSlide />
-        <NavbarApp />
-        <Row>
-          <Col md='8' className='mt-3'>
-            <BeritaList />
-          </Col>
-          <Col>
-            <div className='border border-success p-1 mt-3'>
-              <div className='text-center text-white bg-main-color py-2'>TWITTER</div>
-              <Timeline
-                dataSource={{
-                  sourceType: 'profile',
-                  screenName: 'jawabarat'
-                }}
-                options={{
-                  height: '400'
-                }}/>
-              </div>
-              <StatistikWidget />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <div className='clearfix'></div>
-          <Col md='8'>
-            <BukuTamuWidget />
-          </Col>
-          <Col md='4'>
-            <ExternalWebLink />
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <Header />
+        <Container>
+          <MainSlide />
+          <NavbarApp />
+          <Row>
+            <Col md='8' className='mt-3'>
+              <BeritaList />
+            </Col>
+            <Col>
+              <div className='border border-success p-1 mt-3'>
+                <div className='text-center text-white bg-main-color py-2'>TWITTER</div>
+                <Timeline
+                  dataSource={{
+                    sourceType: 'profile',
+                    screenName: 'jawabarat'
+                  }}
+                  options={{
+                    height: '400'
+                  }}/>
+                </div>
+                <StatistikWidget />
+            </Col>
+          </Row>
+          <hr />
+          <Row>
+            <div className='clearfix'></div>
+            <Col md='8'>
+              <BukuTamuWidget />
+            </Col>
+            <Col md='4'>
+              <ExternalWebLink />
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </div>
     )
   }
 }
