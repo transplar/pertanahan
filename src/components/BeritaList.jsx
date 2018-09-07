34d37ca0851ab7c43086e3bc7b4ebe411b21fea5
zackad <zackad.dev@gmail.com>
2018-09-07 15:01:13 +0700

    BeritaList component

---
+++ src/components/BeritaList.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import loremIpsum from 'lorem-ipsum'
+import Lorem from 'react-lorem-component'
+import { Link } from 'react-router-dom'
+import randomDateFrom from '../utils/random-date-from'
+
+let berita = []
+
+for (let i = 0; i < 10; i++) {
+  berita.push({
+    title: loremIpsum(),
+    date: randomDateFrom('2010'),
+    content: <Lorem count='1' />
+  })
+}
+
+export default class BeritaList extends React.Component {
+  render () {
+    const beritaComponent = berita.map((berita, index) => {
+      return (
+        <div key={index} className='border border-secondary rounded p-2 mb-2'>
+          <h5>{berita.title}</h5>
+          <small>{berita.date.toLocaleString()}</small>
+          {berita.content}
+          <Link to={'/berita/' + index} className='float-right mr-5'>Selengkapnya ...</Link>
+          <div className='clearfix'></div>
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        {beritaComponent}
+      </div>
+    )
+  }
+}
