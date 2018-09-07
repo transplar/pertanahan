882222428a6a375fcefa587e7246eb9b554b67b3
zackad <zackad.dev@gmail.com>
2018-09-07 17:13:47 +0700

    auth handler route

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div className='App'>
           <Route exact path='/' component={Homepage} />
-          <Route path='/signin' component={LoginPage} />
+          <Route path='/:auth(signin|signup)' component={LoginPage} />
         </div>
       </Router>
     )
