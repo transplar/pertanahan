db31f58ad4f501c25a1e9672d4dea3e59a15ab35
zackad <zackad.dev@gmail.com>
2018-09-07 10:20:25 +0700

    use singel quote fo string

--- src/components/StatistikWidget.jsx
+++ src/components/StatistikWidget.jsx
@@ -5,10 +5,11 @@ export default class StatistikWidget extends React.Component {
     return (
       <div className='border border-success p-1 mt-3'>
         <div className='text-uppercase text-center text-white bg-main-color py-2'>Statistik Pengunjung</div>
-        <div id="widgetIframe">
-          <iframe width="100%" height="350"
-          src="https://demo.matomo.org/index.php?module=Widgetize&action=iframe&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&idSite=7&period=day&date=yesterday&disableLink=1&widget=1"
-          scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe>
+        <div id='widgetIframe'>
+          <iframe width='100%' height='350'
+            title='visitor-stats'
+            src='https://demo.matomo.org/index.php?module=Widgetize&action=iframe&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&idSite=7&period=day&date=yesterday&disableLink=1&widget=1'
+            scrolling='no' frameborder='0' marginheight='0' marginwidth='0'></iframe>
         </div>
       </div>
     )
