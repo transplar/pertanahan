a7a12e478ad99b609e5a1aadd1a9e92b202b00b9
zackad <zackad.dev@gmail.com>
2018-09-08 11:35:12 +0700

    route to berita detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import Homepage from './components/layout/Homepage'
+import LoginPage from './components/layout/LoginPage'
+import BeritaDetail from './components/layout/BeritaDetail'
 import './style.css'
-import LoginPage from './components/layout/LoginPage';
 
 class App extends Component {
   render () {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className='App'>
           <Route exact path='/' component={Homepage} />
           <Route path='/:auth(signin|signup)' component={LoginPage} />
+          <Route path='/berita/:id(\d+)' component={BeritaDetail} />
         </div>
       </Router>
     )
