5bf5425a3fd02d8ee8ff76b6ed0e9563a4e0df9d
zackad <zackad.dev@gmail.com>
2018-09-10 09:48:32 +0700

    wrap logo image into div container

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,9 @@ import imageSource from '../images/logo.png'
 export default class Logo extends React.Component {
   render () {
     return (
-      <img className='rounded-0' style={{maxWidth: '128px'}} src={imageSource} alt='Logo Provinsi Jawa Barat' />
+      <div>
+        <img className='header-logo rounded-0' style={{maxWidth: '128px'}} src={imageSource} alt='Logo Provinsi Jawa Barat' />
+      </div>
     )
   }
 }
