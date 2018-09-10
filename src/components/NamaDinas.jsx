794df016bb9d8fbede8caef91fdb708de9f82ec8
zackad <zackad.dev@gmail.com>
2018-09-10 10:05:10 +0700

    fix NamaDinas component to accommodate different screen size

--- src/components/NamaDinas.jsx
+++ src/components/NamaDinas.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Logo from './Logo'
 
 export default class NamaDinas extends React.Component {
   render () {
     return (
-      <div>
-        <h2>Sistem Informasi Pertahanan</h2>
-        <h4>Dinas Perumahan Dan Permukiman Provinsi Jawa Barat</h4>
+      <div className='d-flex flex-column flex-md-row align-items-center flex-grow-1'>
+        <Logo />
+        <div className='d-block d-md-none text-center mb-2'>
+          <h5>Sistem Informasi Pertahanan</h5>
+          <h6>Dinas Perumahan Dan Permukiman Provinsi Jawa Barat</h6>
+        </div>
+        <div className='d-none d-md-block'>
+          <h2>Sistem Informasi Pertahanan</h2>
+          <h5>Dinas Perumahan Dan Permukiman Provinsi Jawa Barat</h5>
+        </div>
       </div>
     )
   }
