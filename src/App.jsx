aeaee8cef9633fb5702bda47f315fd5ca74c1b19
zackad <zackad.dev@gmail.com>
2018-09-10 19:26:04 +0700

    route to download page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import BeritaDetail from './components/layout/BeritaDetail'
+import DownloadPage from './components/layout/DownloadPage'
 import Homepage from './components/layout/Homepage'
 import InformationPage from './components/layout/InformationPage'
 import LoginPage from './components/layout/LoginPage'
@@ -16,6 +17,7 @@ class App extends Component {
           <Route exact path='/' component={Homepage} />
           <Route path='/:auth(signin|signup)' component={LoginPage} />
           <Route path='/berita/:id(\d+)' component={BeritaDetail} />
+          <Route path='/download' component={DownloadPage} />
           <Route path='/information' component={InformationPage} />
           <Route path='/maps' component={Maps} />
         </div>
