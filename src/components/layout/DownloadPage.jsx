5fbe6a75842aa3e6272d88287c3feda25d9828be
zackad <zackad.dev@gmail.com>
2018-09-10 20:42:07 +0700

    load DownloadList component

--- src/components/layout/DownloadPage.jsx
+++ src/components/layout/DownloadPage.jsx
@@ -3,6 +3,7 @@ import { Container } from 'reactstrap'
 import Header from '../Header'
 import NavbarApp from '../NavbarApp'
 import Footer from '../Footer'
+import DownloadList from '../DownloadList'
 
 export default class DownloadPage extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class DownloadPage extends React.Component {
       <Container>
         <Header />
         <NavbarApp />
+        <DownloadList />
         <Footer />
       </Container>
     )
