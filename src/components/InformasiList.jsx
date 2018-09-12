0e792cffd06cfd025ae8c3392784d96972cf41e1
zackad <zackad.dev@gmail.com>
2018-09-12 12:39:28 +0700

    refactor, put card generation into render method

--- src/components/InformasiList.jsx
+++ src/components/InformasiList.jsx
@@ -8,28 +8,28 @@ import Typography from '@material-ui/core/Typography'
 import BookIcon from '@material-ui/icons/Book'
 import bidangData from '../data/bidang.json'
 
-const cards = bidangData.map((bidang) => {
-  return (
-    <Col sm='6' md='4' className='p-2 my-2'>
-      <Card className='d-flex flex-column' style={{ height: '100%' }}>
-        <CardContent className='flex-grow-1'>
-          <Typography variant='title' style={{fontWeight: '400'}}>
-            {bidang.title}
-          </Typography>
-          <Typography variant='subheading'>
-            {bidang.subtitle}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant='raised' color='primary'><BookIcon />Prosedur</Button>
-        </CardActions>
-      </Card>
-    </Col>
-  )
-})
-
 export default class InformationList extends React.Component {
   render () {
+    const cards = bidangData.map((bidang) => {
+      return (
+        <Col sm='6' md='4' className='p-2 my-2'>
+          <Card className='d-flex flex-column' style={{ height: '100%' }}>
+            <CardContent className='flex-grow-1'>
+              <Typography variant='title' style={{fontWeight: '400'}}>
+                {bidang.title}
+              </Typography>
+              <Typography variant='subheading'>
+                {bidang.subtitle}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant='raised' color='primary'><BookIcon />Prosedur</Button>
+            </CardActions>
+          </Card>
+        </Col>
+      )
+    })
+
     return (
       <Row>
         {cards}
