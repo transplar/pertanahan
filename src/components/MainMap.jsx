ffbdc9d46ba946eddf4520b47338f08280bf33a0
zackad <zackad.dev@gmail.com>
2018-09-22 11:45:33 +0700

    add marker to disperkim location

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import { Container } from 'reactstrap'
 import 'leaflet/dist/leaflet.css'
 import './MainMap.css'
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default class MainMap extends React.Component {
   state = {
-    lat: -6.934980,
-    lng: 107.661670,
+    lat: -6.933927,
+    lng: 107.662110,
     zoom: 8
   }
 
@@ -20,6 +28,13 @@ export default class MainMap extends React.Component {
           <TileLayer
             attribution='<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+          <Marker position={position}>
+            <Popup>
+              <p className='text-center'>
+                Dinas Perumahan Dan Permukiman<br />Provinsi Jawa Barat
+              </p>
+            </Popup>
+          </Marker>
         </Map>
       </Container>
     )
