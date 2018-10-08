b85a8d6e62d4ad96c04691ef98e35d46b032b015
zackad <zackad.dev@gmail.com>
2018-10-08 12:00:19 +0700

    reduce navbar padding, add rounded bottom corner

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -26,7 +26,7 @@ export default class NavbarApp extends Component {
 
   render() {
     return (
-      <Navbar className='bg-main-color' dark expand='md'>
+      <Navbar className='bg-main-color font-weight-bold py-0 rounded-bottom' dark expand='md'>
         <Link to='/' className='navbar-brand text-white text-uppercase d-block d-md-none'>Beranda</Link>
         <NavbarToggler onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
