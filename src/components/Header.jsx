923c431b9a33cd4ab3c5ae9144849c154183516c
zackad <zackad.dev@gmail.com>
2018-10-08 11:06:50 +0700

    header background gradient color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default class Header extends React.Component {
   render () {
     return (
       <header>
-        <div className='d-flex flex-wrap flex-lg-nowrap align-items-center my-3'>
+        <div className='d-flex flex-wrap flex-lg-nowrap align-items-center py-3 rounded gradient-background text-white'>
           <NamaDinas />
           <div className='text-right ml-3 flex-grow-1'>
             <Timestamp />
