6e940976a0e1a36b54051cb206d195ec283588e7
zackad <zackad.dev@gmail.com>
2018-11-05 09:00:31 +0700

    remove react-leaflet from dependency

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import L from 'leaflet'
-import { Map, TileLayer } from 'react-leaflet'
 import { Container } from 'reactstrap'
 import 'leaflet/dist/leaflet.css'
 import './MainMap.css'
@@ -16,20 +15,12 @@ export default class MainMap extends React.Component {
     let map = L.map('leaflet-map').setView([this.state.lat, this.state.lng], this.state.zoom)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    })
-      .addTo(map)
+    }).addTo(map)
   }
 
   render () {
-    const position = [this.state.lat, this.state.lng]
-
     return (
       <Container className='bg-white my-1 py-1 rounded'>
-        <Map center={position} zoom={this.state.zoom} ref='map'>
-          <TileLayer
-            attribution='<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-        </Map>
         <div id='leaflet-map'></div>
       </Container>
     )
