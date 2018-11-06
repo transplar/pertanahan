c69575bbd3ff71f3c17c948ad3a4d5f5e380e9de
zackad <zackad.dev@gmail.com>
2018-11-06 13:17:11 +0700

    update layer on change of layer list selected

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -6,6 +6,12 @@ import './MainMap.css'
 import * as wms from './maps/wms-layer'
 
 export default class MainMap extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.updateLayer = this.updateLayer.bind(this)
+  }
+
   state = {
     lat: -6.933927,
     lng: 107.662110,
@@ -29,9 +35,22 @@ export default class MainMap extends React.Component {
     })
   }
 
+  updateLayer () {
+    const layerList = [...document.querySelectorAll('input:checked')]
+      .map(input => input.value)
+      .join(',')
+    wms.simtanah.setParams({layers: layerList}, false)
+  }
+
   render () {
     const layer = this.state.layers
-      .map(layer => <li key={layer.name}>{layer.title}</li>)
+      .map(layer => {
+        return (
+          <li key={layer.name}>
+            <input type='checkbox' value={layer.name} onChange={this.updateLayer}/>{layer.title}
+          </li>
+        )
+      })
 
     return (
       <Container className='bg-white my-1 py-1 rounded'>
