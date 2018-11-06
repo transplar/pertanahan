b5ae12f3a67c0fed3958ace22032622ec5e17626
zackad <zackad.dev@gmail.com>
2018-11-06 10:25:52 +0700

    add wms layer into map

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -3,12 +3,14 @@ import L from 'leaflet'
 import { Container } from 'reactstrap'
 import 'leaflet/dist/leaflet.css'
 import './MainMap.css'
+import * as wms from './maps/wms-layer'
 
 export default class MainMap extends React.Component {
   state = {
     lat: -6.933927,
     lng: 107.662110,
-    zoom: 8
+    zoom: 8,
+    layers: []
   }
 
   componentDidMount () {
@@ -16,12 +18,23 @@ export default class MainMap extends React.Component {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
+    wms.simtanah.addTo(map)
+
+    let layers = wms.getAvailableLayer()
+
+    this.setState({
+      layers: layers
+    })
   }
 
   render () {
+    const layer = this.state.layers
+      .map(layer => <span key={layer.name}>{layer.title}</span>)
+
     return (
       <Container className='bg-white my-1 py-1 rounded'>
         <div id='leaflet-map'></div>
+        {layer}
       </Container>
     )
   }
