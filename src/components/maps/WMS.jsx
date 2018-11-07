0851fa864d803075a33e3ff9a11c3d25453a615b
zackad <zackad.dev@gmail.com>
2018-11-07 15:32:30 +0700

    put wms capabilities inside WMS component

--- src/components/maps/WMS.jsx
+++ src/components/maps/WMS.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import LayerList from '../LayerList'
 import wmsSource from './wms-source'
-import getAvailableLayer from './wms-capabilities';
 
 export default class WMS extends React.Component {
+  sourceURL = `http://${document.location.hostname}:9090/geoserver/ows?`
+  capabilitiesConfig = {
+    request: 'GetCapabilities',
+    service: 'WMS',
+    version: '1.3.0'
+  }
+
   constructor (props) {
     super(props)
 
@@ -13,11 +19,34 @@ export default class WMS extends React.Component {
   }
 
   componentDidMount () {
-    let layers = getAvailableLayer()
+    let layers = this.getAvailableLayer()
     layers.then(res => {
       this.setState({
         layers: res
       })
+    })
+  }
+
+  getAvailableLayer = () => {
+    return new Promise((resolve, reject) => {
+      const capabilitiesParams = Object.entries(this.capabilitiesConfig)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('&')
+
+      fetch([this.sourceURL, capabilitiesParams].join('&'))
+        .then(responnse => responnse.text())
+        .then(text => {
+          let xml = new DOMParser().parseFromString(text, 'text/xml')
+          let layer = [...xml.querySelectorAll('Layer[queryable]')]
+            .map(item => {
+              return {
+                name: item.querySelector('Name').textContent,
+                title: item.querySelector('Title').textContent
+              }
+            })
+          resolve(layer)
+        })
+        .catch(error => reject(error))
     })
   }
 
