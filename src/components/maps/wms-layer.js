4338df3ecb94bce1f8683108d4b7bf304f18beb2
zackad <zackad.dev@gmail.com>
2018-11-07 12:56:28 +0700

    rename variable

--- src/components/maps/wms-layer.js
+++ src/components/maps/wms-layer.js
@@ -1,12 +1,12 @@
 import wms from 'leaflet.wms'
 
-const source = `http://${document.location.hostname}:9090/geoserver/ows?`
-const config = {
+const sourceURL = `http://${document.location.hostname}:9090/geoserver/ows?`
+const capabilitiesConfig = {
   request: 'GetCapabilities',
   service: 'WMS',
   version: '1.3.0'
 }
-const capabilitiesParams = Object.entries(config)
+const capabilitiesParams = Object.entries(capabilitiesConfig)
   .map(([key, value]) => `${key}=${value}`)
   .join('&')
 
@@ -19,11 +19,11 @@ const wmsOptions = {
   feature_count: 100
 }
 
-let wmsSource = wms.source(source, wmsOptions)
+let wmsSource = wms.source(sourceURL, wmsOptions)
 
 const getAvailableLayer = () => {
   return new Promise((resolve, reject) => {
-    fetch([source, capabilitiesParams].join('&'))
+    fetch([sourceURL, capabilitiesParams].join('&'))
       .then(response => response.text())
       .then(text => {
         let xml = new DOMParser().parseFromString(text, 'text/xml')
