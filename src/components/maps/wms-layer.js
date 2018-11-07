156479eed2505f42f4439c3f6c88b08da33a63bb
zackad <zackad.dev@gmail.com>
2018-11-07 10:28:05 +0700

    export wms layer using leaflet.wms plugin

--- src/components/maps/wms-layer.js
+++ src/components/maps/wms-layer.js
@@ -1,4 +1,7 @@
 import L from 'leaflet'
+import wms from 'leaflet.wms'
+
+console.log(wms)
 
 const source = `http://${document.location.hostname}:9090/geoserver/ows?`
 const config = {
@@ -14,6 +17,17 @@ let simtanah = L.tileLayer.wms(source, {
   version: '1.3.0',
   layers: ''
 })
+
+const wmsOptions = {
+  format: 'image/png',
+  tiled: true,
+  transparent: true,
+  version: '1.3.0',
+  info_format: 'text/html',
+  feature_count: 100
+}
+
+let wmsSource = wms.source(source, wmsOptions)
 
 const getAvailableLayer = () => {
   return new Promise((resolve, reject) => {
@@ -34,4 +48,4 @@ const getAvailableLayer = () => {
   })
 }
 
-export { simtanah, getAvailableLayer }
+export { simtanah, getAvailableLayer, wmsSource }
