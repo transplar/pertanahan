9838ff8e70e4591a8d9a9eb1bb38c2df08a6417a
zackad <zackad.dev@gmail.com>
2018-11-26 13:34:14 +0700

    [feature] map legend placeholder in Collapse component

--- src/components/maps/LayerListItem.jsx
+++ src/components/maps/LayerListItem.jsx
@@ -1,28 +1,51 @@
 import React from 'react'
 import Checkbox from '@material-ui/core/Checkbox'
+import Collapse from '@material-ui/core/Collapse'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import IconButton from '@material-ui/core/IconButton'
 import ListItem from '@material-ui/core/ListItem'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
 
 export default class LayerListItem extends React.Component {
+  state = {
+    open: true
+  }
+
+  toggleCollapse = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
   render () {
     return (
-      <ListItem
-        key={this.props.layer.name}
-        className='p-0 border-bottom'
-        dense
-        >
-        <FormControlLabel
-          control={
-            <Checkbox
+      <div className='border-bottom'>
+        <ListItem
+          key={this.props.layer.name}
+          className='p-0'
+          dense
+          >
+          <FormControlLabel
+            control={
+              <Checkbox
               onChange={this.props.onChange}
               value={this.props.layer.name}
               color='primary'
               />
-          }
-          label={this.props.layer.title}
-          className='m-0'
-          />
-      </ListItem>
+            }
+            label={this.props.layer.title}
+            className='m-0'
+            />
+          <span className='flex-grow-1'></span>
+          <IconButton onClick={this.toggleCollapse}>
+            {this.state.open ? <ExpandLess /> : <ExpandMore />}
+          </IconButton>
+        </ListItem>
+        <Collapse in={this.state.open} className='ml-5'>
+          {this.props.layer.legendGraphic}
+        </Collapse>
+      </div>
     )
   }
 }
