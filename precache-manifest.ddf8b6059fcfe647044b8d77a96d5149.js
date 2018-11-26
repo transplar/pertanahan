18095e72cee45fc448974d085e0e7f2fa3ae2ce9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-26 02:52:31 +0000

    Deploy transplar/simtanah-frontend to github.com/transplar/simtanah-frontend.git:gh-pages

---
+++ precache-manifest.ddf8b6059fcfe647044b8d77a96d5149.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "95b7d5aad81ef15eca0563efb89a7a65",
+    "url": "./static/media/contact.95b7d5aa.md"
+  },
+  {
+    "revision": "a6c358f2d0f516a1bc3f",
+    "url": "./static/css/main.cef6b774.chunk.css"
+  },
+  {
+    "revision": "9748f702a1618ac72e50bafc92f663da",
+    "url": "./static/media/profile.9748f702.md"
+  },
+  {
+    "revision": "f3cfcb24f540b5b9783e",
+    "url": "./static/js/1.f3cfcb24.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "efe36345211061c1bd1faa330fbfbd96",
+    "url": "./static/media/logo.efe36345.png"
+  },
+  {
+    "revision": "6bf69e0b519a13ed53680ae201efb8ce",
+    "url": "./static/media/main_banner.6bf69e0b.png"
+  },
+  {
+    "revision": "a6c358f2d0f516a1bc3f",
+    "url": "./static/js/main.a6c358f2.chunk.js"
+  },
+  {
+    "revision": "e756ab785a9b45d4e565116698de2b8a",
+    "url": "./static/media/gallery.e756ab78.md"
+  },
+  {
+    "revision": "3dd3d83401bedfd2efbbcfe0f499fcc6",
+    "url": "./static/media/slide-1.3dd3d834.jpg"
+  },
+  {
+    "revision": "03458b5760941200a193754be0c526a1",
+    "url": "./static/media/slide-2.03458b57.jpg"
+  },
+  {
+    "revision": "3d50915230f709690dc827d95c5c1161",
+    "url": "./static/media/term.3d509152.md"
+  },
+  {
+    "revision": "d7368625f0cf34696363b896524e702e",
+    "url": "./static/media/pengaduan.d7368625.md"
+  },
+  {
+    "revision": "f3cfcb24f540b5b9783e",
+    "url": "./static/css/1.a8e1c080.chunk.css"
+  },
+  {
+    "revision": "c651a744fc30c2ee685ee53c8288d892",
+    "url": "./index.html"
+  }
+];
---
+++ static/media/contact.95b7d5aa.md
@@ -0,0 +1,6 @@
+### Dinas Perumahan Dan Permukiman Jawa Barat
+
+- Jl. Kawaluyaan Indah No. 4, Jatisari, Buahbatu, Kota Bandung 40286
+- telp: 022-7319735
+- email: -
+- website: http://disperkim.jabarprov.go.id
