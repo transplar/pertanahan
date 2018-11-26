3774b958fe0197670cbe2affe8d9aad8d9e4a169
zackad <zackad.dev@gmail.com>
2018-11-26 09:03:41 +0700

    [feature] use material icon as layer list style

--- src/components/maps/LayerList.jsx
+++ src/components/maps/LayerList.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
+import Checkbox from '@material-ui/core/Checkbox'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
 export default class LayerList extends React.Component {
   render () {
     const layer = this.props.layers
       .map(layer => {
         return (
-          <li key={layer.name}>
-            <input type='checkbox' value={layer.name} onChange={this.props.onChange}/>{layer.title}
-          </li>
+          <ListItem
+            key={layer.name}
+            className='p-0 border-bottom'
+            dense
+            disableRipple
+            >
+            <Checkbox
+              onChange={this.props.onChange}
+              value={layer.name}
+              className='p-0'
+              />
+            <ListItemText
+              primary={layer.title}
+              className='p-0'
+              />
+          </ListItem>
         )
       })
 
     return (
-      <ul>{layer}</ul>
+      <List component='nav'>
+        {layer}
+      </List>
     )
   }
 }
