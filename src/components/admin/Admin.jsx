3f1df733b031d4396b544046f3339ea5c5d0394f
zackad <zackad.dev@gmail.com>
2018-11-27 14:56:29 +0700

    [layout] copied template from https://material-ui.com/demos/drawers/#responsive-drawer

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,36 +1,140 @@
 import React from 'react'
-import Appbar from '@material-ui/core/AppBar'
+import AppBar from '@material-ui/core/AppBar'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Divider from '@material-ui/core/Divider'
 import Drawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
 import IconButton from '@material-ui/core/IconButton'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
 import MenuIcon from '@material-ui/icons/Menu'
 
-export default class Admin extends React.Component {
-  render () {
-    return(
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+})
+
+class Admin extends React.Component {
+  state = {
+    mobileOpen: false,
+  }
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }))
+  }
+
+  render() {
+    const { classes, theme } = this.props
+
+    const drawer = (
       <div>
-        <Appbar
-          position='fixed'
-          >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Item'].map(text => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
-            <IconButton>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+              >
               <MenuIcon />
             </IconButton>
-            <Typography>Admin Panel</Typography>
+            <Typography variant="h6" color="inherit" noWrap>
+              Admin Panel
+            </Typography>
           </Toolbar>
-        </Appbar>
-        <Drawer
-          open={true}
-          anchor='left'
-          variant='persistent'
-          >
-          <ul>
-            <li>Items</li>
-          </ul>
-        </Drawer>
-        <div>content</div>
+        </AppBar>
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={this.props.container}
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+              >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...
+          </Typography>
+        </main>
       </div>
     )
   }
 }
+
+export default withStyles(styles, { withTheme: true })(Admin)
