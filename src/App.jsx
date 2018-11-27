064ce77c0fd45346517cff6da5999e6f33cdcc29
zackad <zackad.dev@gmail.com>
2018-11-27 14:04:31 +0700

    [component] add Admin Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import Admin from './components/admin/Admin'
 import BeritaDetail from './components/layout/BeritaDetail'
 import Contact from './components/layout/Contact'
 import DownloadPage from './components/layout/DownloadPage'
