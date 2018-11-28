34db9ff545a0a96f51810cd94f7cafa6e792c706
zackad <zackad.dev@gmail.com>
2018-11-28 13:27:36 +0700

    [refactor] admin drawer as separate component

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,31 +1,16 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Divider from '@material-ui/core/Divider'
-import Drawer from '@material-ui/core/Drawer'
-import Hidden from '@material-ui/core/Hidden'
 import IconButton from '@material-ui/core/IconButton'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
 import MenuIcon from '@material-ui/icons/Menu'
-
-const drawerWidth = 240
+import AdminDrawer, { drawerWidth } from './AdminDrawer'
 
 const styles = theme => ({
   root: {
     display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
   },
   appBar: {
     marginLeft: drawerWidth,
@@ -40,9 +25,6 @@ const styles = theme => ({
     },
   },
   toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
@@ -60,23 +42,6 @@ class Admin extends React.Component {
 
   render() {
     const { classes, theme } = this.props
-
-    const drawer = (
-      <div>
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Item'].map(text => (
-            <ListItem button key={text}>
-              <ListItemIcon className='m-0'>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    )
 
     return (
       <div className={classes.root}>
@@ -96,36 +61,11 @@ class Admin extends React.Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <nav className={classes.drawer}>
-          <Hidden smUp implementation='css'>
-            <Drawer
-              container={this.props.container}
-              variant='temporary'
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={this.state.mobileOpen}
-              onClose={this.handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-              >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation='css'>
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              variant='permanent'
-              open
-              >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
+        <AdminDrawer
+          theme={theme}
+          mobileOpen={this.state.mobileOpen}
+          handleDrawerToggle={this.handleDrawerToggle}
+          />
         <main className={classes.content}>
           <div className={classes.toolbar} />
           <Typography paragraph>
---
+++ src/components/admin/AdminDrawer.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Drawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import { withStyles } from '@material-ui/core/styles'
+
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+})
+
+class AdminDrawer extends React.Component {
+  render () {
+    const { classes, theme } = this.props
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Item'].map(text => (
+            <ListItem button key={text}>
+              <ListItemIcon className='m-0'>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
+
+    return(
+      <nav className={classes.drawer}>
+        <Hidden smUp implementation='css'>
+          <Drawer
+            container={this.props.container}
+            variant='temporary'
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={this.props.mobileOpen}
+            onClose={this.props.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation='css'>
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant='permanent'
+            open
+            >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+    )
+  }
+}
+
+export { drawerWidth }
+export default withStyles(styles, {withTheme: true})(AdminDrawer)
