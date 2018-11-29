2f4176f35a3487aa12c0f62bc39f315070383f41
zackad <zackad.dev@gmail.com>
2018-11-29 11:53:13 +0700

    [component] add input field to NewsEditor

--- src/components/admin/news/NewsEditor.jsx
+++ src/components/admin/news/NewsEditor.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 class NewsEditor extends React.Component {
+  state = {
+    form: []
+  }
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      form: {...this.state.form, [name]: value}
+    })
+  }
+
+  handleSubmit = event => {
+    console.log(this.state.form)
+    event.preventDefault()
+  }
+
   render () {
     return(
-      <div>Editor</div>
+      <form
+        onSubmit={this.handleSubmit}
+        autoComplete='off'
+        style={{maxWidth: '800px'}}
+        action=''
+        method='get'
+        >
+        <TextField
+          fullWidth
+          required
+          onChange={this.handleInputChange}
+          name='title'
+          label='Judul Berita'
+          />
+        <TextField
+          required
+          fullWidth
+          multiline
+          rows={15}
+          onChange={this.handleInputChange}
+          name='content'
+          label='Isi Berita'
+          />
+        <Button type='submit' variant='outlined'>Simpan</Button>
+      </form>
     )
   }
 }
