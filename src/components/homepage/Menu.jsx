da63233102f3393bb181cb884fa6341148626f9f
zackad <zackad.dev@gmail.com>
2018-12-14 00:23:00 +0700

    [style] change hexagon background menu

--- src/components/homepage/Menu.jsx
+++ src/components/homepage/Menu.jsx
@@ -13,6 +13,7 @@ import {
   faUserTie
 } from '@fortawesome/free-solid-svg-icons'
 import { withStyles } from '@material-ui/core/styles'
+import * as icon from './assets'
 
 const styles = theme =>({
   hexgrid: {
@@ -27,54 +28,63 @@ class Menu extends React.Component {
         color: '#009543',
         text: 'BERANDA',
         link: '/',
+        icon: icon.beranda,
         faicon: <FontAwesomeIcon icon={faHome} />
       },
       {
         color: '#029142',
         text: 'PROFIL',
         link: '/profile',
+        icon: icon.profil,
         faicon: <FontAwesomeIcon icon={faUserTie} />
       },
       {
         color: '#058c41',
         text: 'PELAYANAN',
         link: '/layanan',
+        icon: icon.layanan,
         faicon: <FontAwesomeIcon icon={faUserCircle} />
       },
       {
         color: '#037234',
         text: 'PENGADUAN',
         link: '/aduan',
+        icon: icon.aduan,
         faicon: <FontAwesomeIcon icon={faArchway} />
       },
       {
         color: '#066d33',
         text: 'UNDUH',
         link: '/unduh',
+        icon: icon.unduh,
         faicon: <FontAwesomeIcon icon={faCloudDownloadAlt} />
       },
       {
         color: '#086832',
         text: 'GALERI',
         link: '/galeri',
+        icon: icon.galeri,
         faicon: <FontAwesomeIcon icon={faImages} />
       },
       {
         color: '#0c6834',
         text: 'INFORMASI SPASIAL',
         link: '/maps',
+        icon: icon.informasi,
         faicon: <FontAwesomeIcon icon={faInfoCircle} />
       },
       {
         color: '#024921',
         text: 'KONTAK ',
         link: '/kontak',
+        icon: icon.kontak,
         faicon: <FontAwesomeIcon icon={faPhoneSquare} />
       },
       {
         color: '#044420',
         text: 'LOGIN',
         link: '/login',
+        icon: icon.login,
         faicon: <FontAwesomeIcon icon={faSignInAlt} />
       },
     ]
@@ -88,7 +98,8 @@ class Menu extends React.Component {
           <li className="hex" key={list_menu.link}>
             <div className="hexIn">
               <Link to={list_menu.link} className="hexLink" style={{ backgroundColor: list_menu.color }}>
-                {list_menu.faicon}
+                <img src={list_menu.icon} alt=''/>
+                {/* {list_menu.faicon} */}
                 <p>{list_menu.text}</p>
               </Link>
             </div>
---
+++ src/components/homepage/assets/index.js
@@ -0,0 +1,21 @@
+import aduan from './icon_aduan.png'
+import beranda from './icon_beranda.png'
+import galeri from './icon_galeri.png'
+import informasi from './icon_informasi.png'
+import kontak from './icon_kontak.png'
+import layanan from './icon_layanan.png'
+import login from './icon_login.png'
+import profil from './icon_profil.png'
+import unduh from './icon_unduh.png'
+
+export {
+  aduan,
+  beranda,
+  galeri,
+  informasi,
+  kontak,
+  layanan,
+  login,
+  profil,
+  unduh
+}
