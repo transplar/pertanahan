735b4be0e139a3409a1c7496675dac48fd5d875a
zackad <zackad.dev@gmail.com>
2018-12-14 22:21:46 +0700

    [style] homepage cover background

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -3,9 +3,15 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { withStyles } from '@material-ui/core/styles'
 import HomepageHeader from './HomepageHeader'
 import HomepageMenu from './HomepageMenu'
+import bg from './assets/bg_body.jpg'
 import './styles.scss'
 
 const styles = theme => ({
+  background: {
+    background: `url(${bg}) no-repeat center center fixed`,
+    backgroundSize: 'cover',
+    minHeight: '100vh',
+  },
   grow: {
     flexGrow: 1,
   },
@@ -36,7 +42,7 @@ class Homepage extends React.Component {
     const { classes } = this.props
 
     return (
-      <div>
+      <div className={classes.background}>
         <CssBaseline />
         <HomepageHeader />
         <div className={classes.toolbar}></div>
