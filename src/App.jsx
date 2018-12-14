303016393e41b2a732d758d208696cdcb46a66bb
zackad <zackad.dev@gmail.com>
2018-12-14 10:43:26 +0700

    [cleanup] remove unused route

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@ import React, { Component } from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import Admin from './components/admin/Admin'
-import BeritaDetail from './components/layout/BeritaDetail'
-import Contact from './components/layout/Contact'
-import DownloadPage from './components/layout/DownloadPage'
 import Frontpage from './components/frontpage/Frontpage'
-import Gallery from './components/layout/Gallery'
 import Homepage from './components/homepage/Homepage'
-import InformationPage from './components/layout/InformationPage'
 import Login from './components/login/Login'
 import Logout from './components/login/Logout'
-import Pengaduan from './components/layout/Pengaduan'
-import Profile from './components/layout/Profile'
 import './style.css'
 
 class App extends Component {
@@ -23,13 +16,6 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Homepage} />
             <Route path='/admin/:page?' component={Admin} />
-            <Route path='/profil' component={Profile} />
-            <Route path='/berita/:id(\d+)' component={BeritaDetail} />
-            <Route path='/download' component={DownloadPage} />
-            <Route path='/pelayanan' component={InformationPage} />
-            <Route path='/pengaduan' component={Pengaduan} />
-            <Route path='/gallery' component={Gallery} />
-            <Route path='/contact' component={Contact} />
             <Route path='/login' component={Login} />
             <Route path='/logout' component={Logout} />
             <Route path='/:frontpage' component={Frontpage} />
