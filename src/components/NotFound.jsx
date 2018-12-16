280fc86ace995f49f542bbcef888714183be3189
zackad <zackad.dev@gmail.com>
2018-12-16 12:23:39 +0700

    [rewrite] remove bootstrap styling from NotFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {
-  Container,
-} from 'reactstrap'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 import imageSource from '../images/logo.png'
 
-export default class NotFound extends React.Component {
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 5,
+  },
+  imageLogo: {
+    maxHeight: '72px',
+    margin: theme.spacing.unit * 2,
+  },
+})
+
+class NotFound extends React.Component {
   render () {
+    const { classes } = this.props
     return (
-      <Container className='d-flex flex-column align-items-center bg-white rounded my-5 p-5'>
-        <img src={imageSource} alt='Logo Jawa Barat' height='72' className='mb-4' />
-        <h1>Halaman Tidak Ditemukan</h1>
-        <footer>
-          &copy; 2018, <Link to='/'>Kembali ke Beranda</Link>
-        </footer>
-      </Container>
+      <Paper className={classes.root}>
+        <Typography align='center'>
+          <img src={imageSource} alt='Logo Jawa Barat' className={classes.imageLogo} />
+          <h1>Halaman Tidak Ditemukan</h1>
+          <footer>
+            &copy; 2018, <Link to='/'>Kembali ke Beranda</Link>
+          </footer>
+        </Typography>
+      </Paper>
     )
   }
 }
+
+export default withStyles(styles)(NotFound)
