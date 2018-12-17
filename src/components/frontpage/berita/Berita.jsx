652167153ac8305aa120188f8b45a610e16d9391
zackad <zackad.dev@gmail.com>
2018-12-17 11:47:56 +0700

    [layout] fix berita list page

--- src/components/frontpage/berita/Berita.jsx
+++ src/components/frontpage/berita/Berita.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 import { baseAPIURL } from '../../../utils/config'
+
+const styles = theme => ({
+  root: {
+    maxWidth: '1200px',
+    margin: '0 auto',
+  },
+  grow: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+})
 
 class Berita extends React.Component {
   state = {
@@ -24,6 +38,7 @@ class Berita extends React.Component {
 
   render () {
     const { berita, error } = this.state
+    const { classes } = this.props
 
     if (error) {
       return <Typography
@@ -45,28 +60,37 @@ class Berita extends React.Component {
     }
 
     const beritaComponent = berita.items.map((berita, index) => {
+      const dateOptions = {
+        day: 'numeric',
+        weekday: 'long',
+        month: 'long',
+        year: 'numeric'
+      }
+
       return (
-        <div key={berita.id}>
-          <Typography variant='title' color='textSecondary'>
-            <Link to={'/berita/' + berita.id}>{berita.title}</Link>
-          </Typography>
-          <small>{berita.lastUpdate.toLocaleString()}</small>
-          <Typography paragraph>
-            {berita.content}
-          </Typography>
-          <Typography align='right'>
-            <Link to={'/berita/' + index} className='float-right mr-5'>Selengkapnya ...</Link>
-          </Typography>
-        </div>
+        <Grid item sm={12} key={berita.id}>
+          <Paper className={classes.grow}>
+            <Typography variant='title' color='textSecondary'>
+              <Link to={'/berita/' + berita.id}>{berita.title}</Link>
+            </Typography>
+            <small>{(new Date(berita.lastUpdate)).toLocaleDateString('id-ID', dateOptions)}</small>
+            <Typography component='div'>
+              {berita.content}
+            </Typography>
+            <Typography align='right'>
+              <Button component={Link} to={'/berita/' + index}>Selengkapnya ...</Button>
+            </Typography>
+          </Paper>
+        </Grid>
       )
     })
 
     return (
-      <Grid container spacing={8}>
+      <Grid container spacing={8} className={classes.root}>
         {beritaComponent}
       </Grid>
     )
   }
 }
 
-export default Berita
+export default withStyles(styles)(Berita)
