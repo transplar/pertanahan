603a7228a8d793534464294295fa9d7fbfbd8bfa
zackad <zackad.dev@gmail.com>
2018-12-26 13:40:10 +0700

    [fetch] retrieve list items

--- src/components/admin/document/Document.jsx
+++ src/components/admin/document/Document.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import { baseAPIURL } from '../../../utils/config'
 
 const styles = theme => ({
   root: {
-    padding: theme.spacing.unit * 2,
+    padding: theme.spacing.unit,
   },
 })
 
 class Document extends React.Component {
+  state = {
+    documents: [],
+    message: '',
+    error: false
+  }
+
+  componentDidMount () {
+    this.reload()
+  }
+
+  reload = () => {
+    const url = `${baseAPIURL}/document`
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        if (json.status !== 'OK') {
+          this.setState({
+            message: json.message
+          })
+        } else {
+          this.setState({documents: json.items, error: false, message: ''})
+        }
+      })
+      .catch(error => this.setState({message: error.toString()}))
+  }
+
   render () {
     const { classes } = this.props
+    const { message } = this.state
     return(
       <div className={classes.root}>
-        Document
+        <Typography variant='headline' color='error'>
+          {message}
+        </Typography>
       </div>
     )
   }
