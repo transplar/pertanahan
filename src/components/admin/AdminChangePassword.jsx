c33b173a4f42c9ba86a47ead65936f3cca7e5479
zackad <zackad.dev@gmail.com>
2018-12-26 12:48:15 +0700

    [security] submit change password request

--- src/components/admin/AdminChangePassword.jsx
+++ src/components/admin/AdminChangePassword.jsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import { baseAPIURL } from '../../utils/config'
 
 const styles = theme => ({
   root: {
@@ -12,18 +13,47 @@ const styles = theme => ({
   buttonContainer: {
     marginTop: theme.spacing.unit,
   },
+  messageContainer: {
+    padding: theme.spacing.unit,
+  },
 })
 
 class AdminChangePassword extends React.Component {
+  state = {
+    message: ''
+  }
+
+  handleSubmit = event => {
+    const data = {
+      old_password: event.target.old_password.value,
+      new_password: event.target.new_password.value,
+      new_password_confirm: event.target.new_password_confirm.value
+    }
+    const url = `${baseAPIURL}/users/changepassword`
+    fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(data)
+    }).then(response => response.json())
+      .then(json => this.setState({message: json.message}))
+      .catch(error => this.setState({message: error.toString()}))
+    event.target.reset()
+    event.preventDefault()
+  }
+
   render () {
     const { classes } = this.props
+    const { message } = this.state
 
     return(
       <div className={classes.root}>
         <Typography variant='headline'>
           Ganti Password
         </Typography>
-        <form>
+        <Typography className={classes.messageContainer}>
+          {message}
+        </Typography>
+        <form onSubmit={this.handleSubmit}>
           <TextField
             fullWidth
             required
