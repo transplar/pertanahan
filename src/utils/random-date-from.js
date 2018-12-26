1126292047c15608ff38658cb2a00566ca9796a8
zackad <zackad.dev@gmail.com>
2018-12-26 20:52:41 +0700

    [cleanup] remove deadcode

--- src/utils/random-date-from.js
+++
@@ -1,7 +0,0 @@
-const randomDateFrom = function (start) {
-  const now = new Date()
-  const from = new Date(start)
-  return new Date(from.getTime() + Math.random() * (now.getTime() - from.getTime()))
-}
-
-export default randomDateFrom
